aae8ce8a920fe7b845c814329420ea9bf8091ff7
Ryuryu128 <79790536+Ryuryu128@users.noreply.github.com>
2022-12-03 16:24:47 +0900

    Player Animation

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -8,13 +8,67 @@ public class PlayerController : MonoBehaviour
     public Grid _grid;
     public float _speed = 5.0f;
     Vector3Int _cellPos = Vector3Int.zero;
-
-    MoveDir _dir = MoveDir.None;
     bool _isMoving = false;
+    Animator _animator;
 
+    MoveDir _dir = MoveDir.Down;
+
+    public MoveDir Dir
+    {
+        get { return _dir; }
+        set
+        {
+            if (_dir == value)
+                return;
+
+            switch (value)
+            {
+                case MoveDir.Up:
+                    _animator.Play("WALK_BACK");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Down:
+                    _animator.Play("WALK_FRONT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Left:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Right:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.None:
+                    if ( _dir == MoveDir.Up)
+                    {
+                        _animator.Play("IDLE_BACK");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    else if ( _dir == MoveDir.Down)
+                    {
+                        _animator.Play("IDLE_FRONT");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    else if ( _dir == MoveDir.Left)
+                    {
+                        _animator.Play("IDLE_RIGHT");
+                        transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    }
+                    else
+                    {
+                        _animator.Play("IDLE_RIGHT");
+                        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    }
+                    break;
+            }
+            _dir = value;
+        }
+    }
 
     void Start()
     {
+        _animator = GetComponent<Animator>();
         Vector3 pos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
         transform.position = pos;
     }
@@ -24,9 +78,34 @@ public class PlayerController : MonoBehaviour
         GetDirInput();
         UpdatePosition();
         UpdateIsMoving();
-        
+
     }
     // 키보드 입력을 받아 방향을 정하는 함수
+    void GetDirInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Dir = MoveDir.Up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Dir = MoveDir.Down;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Dir = MoveDir.Left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Dir = MoveDir.Right;
+        }
+        else
+        {
+            Dir = MoveDir.None;
+        }
+    }
+
+    // 시각적인 이동을 구현하는 함수 (실제 이동x)
     void UpdatePosition()
     {
         if (_isMoving == false)
@@ -38,41 +117,17 @@ public class PlayerController : MonoBehaviour
         // 도착 여부 체크
         // if조건문 : 이동해야하는 거리(목적지까지의 거리 dist)가 한 프레임에 움직일 수 있는 거리보다 작다면 도착했다고 간주
         float dist = moveDir.magnitude;
-        if (dist< _speed * Time.deltaTime)
+        if (dist < _speed * Time.deltaTime)
         {
             transform.position = destPos;
             _isMoving = false;
         }
-        else 
+        else
         {
             transform.position += moveDir.normalized * _speed * Time.deltaTime;
         }
     }
-
-    // 시각적인 이동을 구현하는 함수 (실제 이동x)
-    void GetDirInput()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            _dir = MoveDir.Up;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            _dir = MoveDir.Down;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            _dir = MoveDir.Left;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            _dir = MoveDir.Right;
-        }
-        else
-        {
-            _dir = MoveDir.None;
-        }
-    }
+    
 
     // 이동가능한 상태일 때 실질적인 위치인 _cellPos를 움직이는 함수
     void UpdateIsMoving()
